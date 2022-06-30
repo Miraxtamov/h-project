68792ff18004b61e77fb806592fbbc38472ca9ef
Miraxtamov <jmiraxtamov@gmail.com>
2022-06-30 16:44:35 +0500

    start project

---
+++ src/root/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Router, Routes, Route} from "react-router-dom";
+
+const Root = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Root;
